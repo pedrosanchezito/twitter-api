019041d2f6ee014864e2964313893cce70c4297b
pedrosanchezito <ps8560251@gmail.com>
2019-04-08 23:52:20 +0200

    added register and login routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, render_template
 from flask_restplus import Api
 from flask_login import LoginManager
 
@@ -12,6 +12,10 @@ def create_app():
     app.config.from_object(Config)
     db.init_app(app)
 
+    @app.route('/hello')
+    def hello():
+        return "Goodbye World!"
+
     login_manager = LoginManager()
     login_manager.init_app(app)
     login_manager.login_view = 'login'
@@ -19,9 +23,21 @@ def create_app():
     def load_user(id):
         return User.query.get(int(id))
 
-    @app.route('/hello')
-    def hello():
-        return "Goodbye World!"
+    @app.route('/register' , methods=['GET','POST'])
+    def register():
+        if request.method == 'GET':
+            return render_template('register.html')
+        user = User(request.form['username'] , request.form['password'],request.form['email'])
+        db.session.add(user)
+        db.session.commit()
+        flash('User successfully registered')
+        return redirect(url_for('login'))
+
+    @app.route('/login',methods=['GET','POST'])
+    def login():
+        if request.method == 'GET':
+            return render_template('login.html')
+        return redirect(url_for('index'))
 
     from .apis.tweets import api as tweets
     api = Api()
